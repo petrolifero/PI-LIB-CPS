15c8fb75cc70906166df335a52dcd604a28110d3
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-09-05 10:05:32 -0300

    uma parte das expressões booleanas

--- main.rkt
+++ main.rkt
@@ -4,6 +4,9 @@
 (struct mul (a b) #:transparent)
 (struct div (a b) #:transparent)
 
+(struct orS (a b) #:transparent)
+(struct andS (a b) #:transparent)
+
 (define (+& a b sucesso falha )
     (if (and (number? a) (number? b))
         (sucesso (+ a b))
@@ -24,6 +27,22 @@
         (sucesso (/ a b))
         (falha "deu merda")))
 
+(define (or& a b sucesso falha )
+    (if (and (boolean? a) (boolean? b))
+        (sucesso (or a b))
+        (falha "deu merda")))
+
+(define (and& a b sucesso falha )
+    (if (and (boolean? a) (boolean? b))
+        (sucesso (and a b))
+        (falha "deu merda")))
+
+(define (>=& a b sucesso falha )
+    (if (and (number? a) (number? b))
+        (sucesso (>= a b))
+        (falha "deu merda")))
+
+
 (struct ambiente (amb mem loca) #:transparent)
 
 (define (eval-pi-lib exp ambiente sucesso falha)
@@ -33,6 +52,13 @@
         [(add a b) (eval-pi-lib a ambiente (lambda (v1) (eval-pi-lib b ambiente (lambda (v2) (+& v1 v2 sucesso falha)) falha)) falha)]
         [(sub a b) (eval-pi-lib a ambiente (lambda (v1) (eval-pi-lib b ambiente (lambda (v2) (-& v1 v2 sucesso falha)) falha)) falha)]
         [(mul a b) (eval-pi-lib a ambiente (lambda (v1) (eval-pi-lib b ambiente (lambda (v2) (*& v1 v2 sucesso falha)) falha)) falha)]
-        [(div a b) (eval-pi-lib a ambiente (lambda (v1) (eval-pi-lib b ambiente (lambda (v2) (/& v1 v2 sucesso falha)) falha)) falha)]))
+        [(div a b) (eval-pi-lib a ambiente (lambda (v1) (eval-pi-lib b ambiente (lambda (v2) (/& v1 v2 sucesso falha)) falha)) falha)]
+	[(orS a b) (eval-pi-lib a ambiente (lambda (v1) (eval-pi-lib b ambiente (lambda (v2) (or& v1 v2 sucesso falha)) falha)) falha)]
+	[(andS a b) (eval-pi-lib a ambiente (lambda (v1) (eval-pi-lib b ambiente (lambda (v2) (and& v1 v2 sucesso falha)) falha)) falha)]
+	[(ge a b) (eval-pi-lib a ambiente (lambda (v1) (eval-pi-lib b ambiente (lambda (v2) (>=& v1 v2 sucesso falha)) falha)) falha)]
+	[(gt a b) ()]
+	[(lt a b)]
+	[(neg a)]
+	[(eq a b)]
+	[(le a b)]))
 
-(eval-pi-lib (mul (div 1 3) 2) (ambiente (hash) (hash) '()) display display)
